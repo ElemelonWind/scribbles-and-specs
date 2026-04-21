26c2137bd9082cd3d994a76d570aa3874b243353
Artyom Boyarov <61147389+artyom-boyarov@users.noreply.github.com>
2026-04-21 13:55:00 -0400

    Added PID + Feedforward control through a linear model of motor PWM -> wheel speed. Tuned gains to get good unit response.

--- Scribbles/include/PID_FF.hpp
+++ Scribbles/include/PID_FF.hpp
@@ -1,18 +1,24 @@
 #pragma once
 #include <Arduino.h>
 
-class PID {
+class PID_FF {
 public:
-    PID(float kp, float ki, float kd);
+    PID_FF(float m, float b, float kp, float ki, float kd);
 
     // Computes the controller output for the given setpoint and measurement.
     // dt is the elapsed time since the previous update in seconds.
+
+    // Calculates feedforward term as output = m * setpoint + b, where m is the slope and b is the intercept.
+    // Adds an error PID term.
+
     float update(float setpoint, float measurement);
 
     // Resets the integral and derivative history.
     void reset();
 
 private:
+    float m;
+    float b;
     float kp;
     float ki;
     float kd;
--- Scribbles/src/main.cpp
+++ Scribbles/src/main.cpp
@@ -1,46 +1,65 @@
 #include <Arduino.h>
 #include "HBridgeMotor.hpp"
 #include <ESP32Encoder.h>
-#include "PID.hpp"
+#include "PID_FF.hpp"
 #include "WheelSpeed.hpp"
 #include "ESP32_PWM.h"
 
-HBridgeMotor motor0(5, 18, 19);
-HBridgeMotor motor1(32, 33, 25);
-HBridgeMotor motor2(26, 27, 14);
+int cnt = 0;
 
-PID pid0(1.0, 0.0, 0.0);
-PID pid1(1.0, 0.0, 0.0);
-PID pid2(1.0, 0.0, 0.0);
+HBridgeMotor motor0(27, 25, 26);
+HBridgeMotor motor1(13, 12, 23);
+HBridgeMotor motor2(32, 33, 18);
+
+PID_FF pid0(0.08929, 0.0188, 0.02, 0.1, 0.002);
+PID_FF pid1(0.08105, 0.02559, 0.02, 0.1, 0.002);
+PID_FF pid2(0.08084, 0.02601, 0.02, 0.1, 0.002);
 
 
 WheelSpeed wheelSpeed0;
 WheelSpeed wheelSpeed1;
 WheelSpeed wheelSpeed2;
 
-float targetSpeed = 0.2f;
+float targetSpeed = 0.5f;
 
 
 void setup() {
 
-  wheelSpeed0 = WheelSpeed(2, 4);
-  wheelSpeed1 = WheelSpeed(13, 17);
-  wheelSpeed2 = WheelSpeed(22, 23);
+  wheelSpeed0 = WheelSpeed(4, 5);
+  wheelSpeed1 = WheelSpeed(21, 22);
+  wheelSpeed2 = WheelSpeed(14, 19);
 
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, 16, 17);
 }
 
 void loop() {
-  // Encoder - PID test
+  // Encoder - PID_FF test
 
-  motor0.set(pid0.update(targetSpeed, wheelSpeed0.calculateSpeed()));
-  motor1.set(pid1.update(targetSpeed, wheelSpeed1.calculateSpeed()));
-  motor2.set(pid2.update(targetSpeed, wheelSpeed2.calculateSpeed()));
+  if (cnt > 25) {
+    motor0.stop();
+    motor1.stop();
+    motor2.stop();
+    return;
+  }
+  cnt++;
 
-  Serial.print(wheelSpeed0.calculateSpeed());
-  Serial.print(", ");
-  Serial.print(wheelSpeed1.calculateSpeed());
-  Serial.print(", ");
-  Serial.println(wheelSpeed2.calculateSpeed());
+  float wheel0Speed = wheelSpeed0.calculateSpeed();
+  float wheel1Speed = wheelSpeed1.calculateSpeed();
+  float wheel2Speed = wheelSpeed2.calculateSpeed();
+
+  float motor0Output = pid0.update(targetSpeed, wheel0Speed);
+  float motor1Output = pid1.update(targetSpeed, wheel1Speed);
+  float motor2Output = pid2.update(targetSpeed, wheel2Speed);
+
+  if (abs(motor0Output) < 0.05f) motor0.stop();
+  else motor0.set(abs(motor0Output), motor0Output >= 0.0f);
+
+  if (abs(motor1Output) < 0.05f) motor1.stop();
+  else motor1.set(abs(motor1Output), motor1Output >= 0.0f);
+
+  if (abs(motor2Output) < 0.05f) motor2.stop();
+  else motor2.set(abs(motor2Output), motor2Output >= 0.0f);
+
+  delay(200);
 }
